c5772d6e7c7ef813bbc9adc3b9cfe6c18f7ffde1
Freeside Jockey <freesidejockey@gmail.com>
2025-09-17 09:45:55 -0600

    feat: initial implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-amber-100">
-      {/* Background */}
-      <div className="max-w-md mx-auto bg-white">
-        {/* Card*/}
-        <div className="relative w-full h-64">
-          {" "}
-          {/* Make container relative with desired height */}
-          <Image
-            src="/images/image-qr-code.png"
-            alt="QR Code"
-            fill
-            className="object-contain" // or object-cover depending on your needs
-          />
-        </div>
-        {/* Card Image or Header */}
-        <div>
-          {/* Card Body */}
-          <p>
-            {/* Main Text */}
-            Improve your front-end skills by building projects
-          </p>
-          <p>
-            {/* Sub Text */}
-            Scan the QR code to visit Frontend Mentor and take your coding
-            skills to the next level
-          </p>
+    <article className="h-screen bg-slate-300">
+      <div className="mx-auto pb-8"></div>
+      <div className="mx-auto max-w-sm">
+        <div className="mr-8 mb-8 ml-8 rounded-xl bg-red-50 p-4">
+          <picture>
+            <img
+              className="rounded-xl"
+              src="/images/image-qr-code.png"
+              alt="QR Code"
+            />
+          </picture>
+
+          <div className="mx-auto max-w-xs">
+            <div className="pt-6 pb-2 text-center text-lg font-extrabold">
+              Improve your front-end skills by building projects
+            </div>
+            <p className="pb-2 text-center text-sm font-thin">
+              Scan the QR code to visit Frontend Mentor and take your coding
+              skills to the next level
+            </p>
+          </div>
         </div>
       </div>
       <div className="attribution">
@@ -35,8 +30,9 @@ export default function Home() {
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>{" "}
-    </div>
+        . Coded by{" "}
+        <a href="https://github.com/freesidejockey">FreesideJockey</a>.
+      </div>
+    </article>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        slate: {
+          300: "hsl(212, 45%, 89%)",
+          500: "hsl(216, 15%, 48%)",
+          900: "hsl(218, 44%, 22%)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
